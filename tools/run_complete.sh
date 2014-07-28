329b2945e66e9da3ed2c86f9d2c22fb62a931813
Matthew Taylor <rhyolight@gmail.com>
2014-07-28 14:51:38 -0700

    Using dirname(${0}) as suggested

--- tools/run_complete.sh
+++ tools/run_complete.sh
@@ -5,7 +5,7 @@ if [ "$#" -ne 1 ]; then
     exit
 fi
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SCRIPT_DIR=$(dirname "${0}")
 
 OUTPUT="output"
 PROCESSED_DATA=${OUTPUT}/processed_data.csv
