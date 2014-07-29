221f2d7579e856cadf7bf61169536a1d1a94aeae
Matthew Taylor <rhyolight@gmail.com>
2014-07-29 12:15:25 -0700

    Adding speed

--- tools/convert_gpx.py
+++ tools/convert_gpx.py
@@ -2,15 +2,14 @@
 import sys
 import os
 import csv
+import math
 import time
 from optparse import OptionParser
 
 import gpxpy
 import gpxpy.gpx
 
-# INPUT = './data/Mon Jul 28 2014.gpx'
 DEFAULT_OUTPUT_DIR = "output"
-# OUTPUT = './data/convert_gpx_output.csv'
 verbose = False
 
 parser = OptionParser(
@@ -35,22 +34,59 @@ parser.add_option(
 )
 
 
+def distance_on_unit_sphere_in_meters(point1, point2):
+  lat1 = point1.latitude
+  long1 = point1.longitude
+  lat2 = point2.latitude
+  long2 = point2.longitude
+
+  # Convert latitude and longitude to
+  # spherical coordinates in radians.
+  degrees_to_radians = math.pi/180.0
+
+  # phi = 90 - latitude
+  phi1 = (90.0 - lat1)*degrees_to_radians
+  phi2 = (90.0 - lat2)*degrees_to_radians
+
+  # theta = longitude
+  theta1 = long1*degrees_to_radians
+  theta2 = long2*degrees_to_radians
+
+  cos = (math.sin(phi1)*math.sin(phi2)*math.cos(theta1 - theta2) +
+         math.cos(phi1)*math.cos(phi2))
+  arc = math.acos( cos )
+
+  # Multiplied to get M
+  return arc * 6373000
+
+
 def run(input_path, output_dir):
 
   gpx_file = open(input_path, 'r')
   gpx = gpxpy.parse(gpx_file)
 
   output_file = os.path.join(output_dir, "converted_gpx_output.csv")
+  last_point = None
 
   with open(output_file, 'w') as file_out:
     writer = csv.writer(file_out)
     for track in gpx.tracks:
+      if verbose:
+        print track.name
       for segment in track.segments:
         for point in segment.points:
           ts = int(time.mktime(point.time.timetuple()) * 1000)
+          meters_per_second = 0
+          if last_point:
+            distance_travelled = distance_on_unit_sphere_in_meters(last_point, point)
+            ms_since_last_point = ts - int(time.mktime(last_point.time.timetuple()) * 1000)
+            if ms_since_last_point > 0:
+              meters_per_second = distance_travelled / (ms_since_last_point / 1000)
+
           if verbose:
             print "{0}: ({1},{2})".format(point.time.__str__(), point.latitude, point.longitude)
-          writer.writerow(["testroute", ts, point.longitude, point.latitude, None, 0, None, 1])
+          writer.writerow([track.name, ts, point.longitude, point.latitude, None, meters_per_second, None, 1])
+          last_point = point
 
 
 if __name__ == "__main__":
